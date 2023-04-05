0fe47abc8deb5ca55fe79eae7b48cf5edf70fc0d
dulalsaurab <sdulal@memphis.edu>
2023-04-04 21:05:30 -0500

    add random suppression timer

--- daemon/face/link-service.cpp
+++ daemon/face/link-service.cpp
@@ -28,6 +28,8 @@
 
 namespace nfd::face {
 
+uint8_t MAX_PROPAGATION_DELAY = 30;
+
 NFD_LOG_INIT(LinkService);
 
 LinkService::~LinkService() = default;
@@ -54,24 +56,17 @@ LinkService::sendInterest(const Interest& interest)
     doSendInterest(interest);
     return;
   }
-  // apply suppression algorithm if sending through multicast face
-  // check if the interest is already in flight
-  if (m_multicastSuppression.interestInflight(interest)) {
-    NFD_LOG_INFO ("Interest drop, Interest " <<  interest.getName() << " is in flight, drop the forwarding");
-    return; // need to catch this, what should be the behaviour after dropping the interest?? 
-  }
-  // wait for suppression time before forwarding
-  // check if another interest is overheard during the wait, if heard, cancle the forwarding
-  auto suppressionTime = m_multicastSuppression.getDelayTimer(interest.getName(), 'i');
-  NFD_LOG_INFO ("Interest " <<  interest.getName() << " not in flight, waiting" << suppressionTime << "before forwarding");
+
+  time::milliseconds suppressionTimer (nfd::face::ams::getRandomNumber(MAX_PROPAGATION_DELAY)); // timer is randomized value
+  NFD_LOG_INFO ("Scheduling interest: " <<  interest.getName() << " to forward after: " << suppressionTimer);
 
   auto entry_name = interest.getName();
   entry_name.appendNumber(0);
-  auto eventId = getScheduler().schedule(suppressionTime, [this, interest, entry_name] {
+
+  auto eventId = getScheduler().schedule(suppressionTimer, [this, interest, entry_name] {
     NFD_LOG_INFO ("Interest " <<  interest.getName() << " sent, finally");
     ++this->nOutInterests;
     doSendInterest(interest);
-    m_multicastSuppression.recordInterest(interest, true);
     if (m_scheduledEntry.count(entry_name) > 0)
       m_scheduledEntry.erase(entry_name);
   });
@@ -89,31 +84,18 @@ LinkService::sendData(const Data& data)
     doSendData(data);
     return;
   }
-  /*
-    Same suppression logic as that of interest cannot be applied to multicast data.
-    Doing so we might end up suppressing data for the interest received at different interval
-    from different node, additionally it will also trigger multiple retransmission from the node
-    that didn't received the data due to suppression. This might not happen if unsolicated data are
-    cached, but the node that's supposed to send the data can't gurantee it.
-
-    data sending should wait before forwarding, during this wait time if another data is overheard, need to drop the forwarding
-    wait time should be determined based on the number of duplicate overhearing
-  */
-  //  for now, lets wait for some time before forwarding, if overheard, drop the reply
-  auto suppressionTime = m_multicastSuppression.getDelayTimer(data.getName(), 'd');
-  NFD_LOG_INFO("Waiting : " << suppressionTime<< "ms before sending data" << data.getName());
+  time::milliseconds suppressionTimer (nfd::face::ams::getRandomNumber(MAX_PROPAGATION_DELAY)); // timer is randomized value
+  NFD_LOG_INFO ("Scheduling data: " <<  data.getName() << " to forward after: " << suppressionTimer);
 
   auto entry_name = data.getName();
   entry_name.appendNumber(1);
-  auto eventId = getScheduler().schedule(suppressionTime, [this, data, entry_name] {
 
-    NFD_LOG_INFO("Sending data finally, via multicast face " << data.getName());
+  auto eventId = getScheduler().schedule(suppressionTimer, [this, data, entry_name] {
+    NFD_LOG_INFO ("Data " <<  data.getName() << " sent, finally");
     ++this->nOutData;
     doSendData(data);
-    m_multicastSuppression.recordData(data, true);
-    if(m_scheduledEntry.count(entry_name) > 0)
-        m_scheduledEntry.erase(entry_name);
-
+    if (m_scheduledEntry.count(entry_name) > 0)
+      m_scheduledEntry.erase(entry_name);
   });
   m_scheduledEntry.emplace(entry_name, eventId);
 }
@@ -153,7 +135,7 @@ LinkService::receiveInterest(const Interest& interest, const EndpointId& endpoin
     // check if a same interest is scheduled, if so drop it
     if (cancelIfSchdeuled(interest.getName(), 0))
       NDN_LOG_INFO("Interest drop, Interest " << interest.getName() << " overheard, duplicate forwarding dropped");
-    m_multicastSuppression.recordInterest(interest, false);
+    // m_multicastSuppression.recordInterest(interest, false);
   }
   ++this->nInInterests;
   afterReceiveInterest(interest, endpoint);
@@ -173,7 +155,7 @@ LinkService::receiveData(const Data& data, const EndpointId& endpoint)
     if (cancelIfSchdeuled(data.getName(), 0)) // also can drop interest if shceduled for this data
       NDN_LOG_INFO("Interest drop, Data " << data.getName() << " overheard, drop the corresponding scheduled interest"); 
     
-    m_multicastSuppression.recordData(data, false);
+    // m_multicastSuppression.recordData(data, false);
   }
 
   ++this->nInData;
